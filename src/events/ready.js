0349ceb54968e7d737d87393923c1fadebf22377
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2022-11-01 01:55:15 -0400

    feat: Form embed creation

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,3 +1,4 @@
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 const config = require("../config");
 
 module.exports = {
@@ -8,5 +9,25 @@ module.exports = {
         const guild = client.guilds.cache.get(config.guild);
         await guild.members.fetch();
         await guild.commands.fetch();
+
+        const formChannel = guild.channels.cache.get(config.formChannel);
+        const message = (await formChannel?.messages?.fetch({ limit: 1 }).catch(() => null))?.first();
+        
+        if (!message || message.author.id !== client.user.id) {
+            const embed = new EmbedBuilder()
+                .setTitle("Embed de form")
+                .setDescription("Se você representa um servidor que cumpre os requisitos listados acima e acha que ele merece fazer parte da EPF, clique no botao abaixo e preencha o formulário para enviá-lo para uma avaliação")
+                .setColor("Random");
+
+            const row = new ActionRowBuilder()
+                .setComponents(
+                    new ButtonBuilder()
+                        .setCustomId("form-button")
+                        .setLabel("Fazer Formulário")
+                        .setStyle(ButtonStyle.Secondary)
+                );
+
+            formChannel.send({ embeds: [embed], components: [row] });
+        }
     }
 }
