6c036846ae8deaa32e1e7d7b556149f374d6517e
HordLawk <22248342+HordLawk@users.noreply.github.com>
2022-11-03 19:35:38 -0300

    fix(serverform): give level role to representative

--- src/components/buttons/aprove-server-form.js
+++ src/components/buttons/aprove-server-form.js
@@ -1,4 +1,5 @@
 const { EmbedBuilder } = require("discord.js");
+const config = require("../../config");
 const guildModel = require("../../models/guild");
 
 module.exports = {
@@ -18,6 +19,18 @@ module.exports = {
 
         guildDoc.pending = false;
         await guildDoc.save();
+
+        if(guildDoc.owner === guildMember.id){
+            await guildMember.roles.add(config.levels[2]);
+        }
+        else{
+            const memberModel = require('../../models/member.js');
+            const memberDoc = await memberModel.findOne({
+                user: guildMember.id,
+                guild: guildDoc._id,
+            });
+            await guildMember.roles.add(config.levels[+memberDoc.admin]);
+        }
 
         const message = await guildMember.send({ content: `Parabéns, o seu servidor \`${guildDoc.name}\` foi aprovado na EPF!` }).catch(() => null);
         if (!message) await interaction.reply({ content: "Não foi possível entrar em contato com o representante do servidor", ephemeral: true });
