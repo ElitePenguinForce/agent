6ba6061c460640c1674ef2a0a7c3ceb6247a8ee8
HordLawk <22248342+HordLawk@users.noreply.github.com>
2022-11-03 16:39:39 -0300

    fix(events): execute should be called with await

--- src/handlers/handleEvents.js
+++ src/handlers/handleEvents.js
@@ -4,6 +4,6 @@ const path = require('path');
 module.exports = async client => {
     fs.readdirSync(path.join(__dirname, '..', 'events')).filter(file => file.endsWith(".js")).forEach(file => {
         const event = require(`../events/${file}`);
-        client[event.once ? 'once' : 'on'](event.name, (...args) => event.execute(...args, client).catch(console.error));
+        client[event.once ? 'once' : 'on'](event.name, async (...args) => await event.execute(...args, client).catch(console.error));
     });
 }
