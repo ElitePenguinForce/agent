0fc8c2b2cceb856bfe81a404c09ad3f69307bf9b
HordLawk <22248342+HordLawk@users.noreply.github.com>
2022-11-15 05:00:35 -0300

    fix: no roles means only the everyone role

--- src/events/ready.js
+++ src/events/ready.js
@@ -80,9 +80,7 @@ module.exports = {
             const expiredDocs = await guestModel.find({expiresAt: {$lt: now}}).limit(100);
             const expiredIds = expiredDocs.map(doc => doc._id);
             const members = await guild.members.fetch({user: expiredIds});
-            console.log(expiredIds);
-            console.log(members);
-            for(const member of members.filter(m => !m.roles.cache.size).values()) await member.kick();
+            for(const member of members.filter(m => (m.roles.cache.size === 1)).values()) await member.kick();
             await guestModel.deleteMany({_id: {$in: expiredIds}});
             clock();
         }, 60 * 60 * 1000);
