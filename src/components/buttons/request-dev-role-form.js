0f7bdb004edc590650c5f8698c47b61554504a96
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2023-01-10 12:47:10 -0400

    refactor: fix indentation

--- src/components/buttons/request-dev-role-form.js
+++ src/components/buttons/request-dev-role-form.js
@@ -1,45 +1,45 @@
 const { ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
 
 module.exports = {
-  data: {
-    name: "request-dev-role-form"
-  },
+    data: {
+        name: "request-dev-role-form"
+    },
 
-  async execute(interaction, client) {
-    const modal = new ModalBuilder()
-      .setCustomId(`devRequestModal`)
-      .setTitle(`Elite Penguin Force`)
+    async execute(interaction, client) {
+        const modal = new ModalBuilder()
+        .setCustomId(`devRequestModal`)
+        .setTitle(`Elite Penguin Force`)
 
-    const githubLink = new TextInputBuilder()
-        .setCustomId(`githubLink`)
-        .setLabel(`O link do seu perfil no GitHub`)
-        .setRequired(true)
-        .setStyle(TextInputStyle.Short)
+        const githubLink = new TextInputBuilder()
+            .setCustomId(`githubLink`)
+            .setLabel(`O link do seu perfil no GitHub`)
+            .setRequired(true)
+            .setStyle(TextInputStyle.Short)
 
-    const experienceInfo = new TextInputBuilder()
-        .setCustomId(`experienceInfo`)
-        .setLabel(`Qual sua atuação na área?`)
-        .setRequired(true)
-        .setPlaceholder("Criação de sites/bots, etc...")
-        .setStyle(TextInputStyle.Paragraph)
+        const experienceInfo = new TextInputBuilder()
+            .setCustomId(`experienceInfo`)
+            .setLabel(`Qual sua atuação na área?`)
+            .setRequired(true)
+            .setPlaceholder("Criação de sites/bots, etc...")
+            .setStyle(TextInputStyle.Paragraph)
 
-    const example = new TextInputBuilder()
-        .setCustomId(`example`)
-        .setLabel(`Nos envie um exemplo de um projeto seu`)
-        .setRequired(true)
-        .setStyle(TextInputStyle.Paragraph)
+        const example = new TextInputBuilder()
+            .setCustomId(`example`)
+            .setLabel(`Nos envie um exemplo de um projeto seu`)
+            .setRequired(true)
+            .setStyle(TextInputStyle.Paragraph)
 
-    const botInvite = new TextInputBuilder()
-        .setCustomId(`botInvite`)
-        .setLabel(`Convite do seu bot (se houver)`)
-        .setStyle(TextInputStyle.Short)
-        .setRequired(false)
+        const botInvite = new TextInputBuilder()
+            .setCustomId(`botInvite`)
+            .setLabel(`Convite do seu bot (se houver)`)
+            .setStyle(TextInputStyle.Short)
+            .setRequired(false)
 
-    modal.addComponents(new ActionRowBuilder().addComponents(githubLink));
-    modal.addComponents(new ActionRowBuilder().addComponents(experienceInfo));
-    modal.addComponents(new ActionRowBuilder().addComponents(example));
-    modal.addComponents(new ActionRowBuilder().addComponents(botInvite));
+        modal.addComponents(new ActionRowBuilder().addComponents(githubLink));
+        modal.addComponents(new ActionRowBuilder().addComponents(experienceInfo));
+        modal.addComponents(new ActionRowBuilder().addComponents(example));
+        modal.addComponents(new ActionRowBuilder().addComponents(botInvite));
 
-    return await interaction.showModal(modal);
-  }
+        return await interaction.showModal(modal);
+    }
 }
