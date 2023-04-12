06192fbcae360f1ac2ddf4880b7abc8ce4cb1ea2
HordLawk <22248342+HordLawk@users.noreply.github.com>
2023-04-12 08:14:26 -0300

    fix(db): connection full string

--- src/database.js
+++ src/database.js
@@ -1,13 +1,7 @@
 const mongoose = require("mongoose");
 const app = mongoose.connection;
 
-const connect = async () => {
-    return (
-        await mongoose
-            .connect(`${process.env.MONGOURL}?retryWrites=true&w=majority`, {useNewUrlParser: true})
-            .catch(console.error)
-    );
-}
+const connect = async () => await mongoose.connect(process.env.MONGOURL).catch(console.error);
 
 app.on("error", async err => {
     console.error(err);
