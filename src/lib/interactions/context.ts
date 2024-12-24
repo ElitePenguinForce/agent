e904947b86df0ca04038f346d218708926208304
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2024-12-24 17:42:21 -0400

    fix: possible access property of undefined error

--- src/lib/interactions/context.ts
+++ src/lib/interactions/context.ts
@@ -3,15 +3,18 @@ import type { ContextMenuCommandInteraction } from "discord.js";
 export default async function handleContextInteraction(
   interaction: ContextMenuCommandInteraction,
 ) {
-  const context = interaction.client.contexts[interaction.type].get(
-    interaction.commandName,
-  );
+  const contexts = interaction.client.contexts[interaction.type];
+  if (!contexts) {
+    return;
+  }
+
+  const context = contexts.get(interaction.commandName);
   if (!context) {
     return;
   }
 
   try {
-    // @ts-expect-error
+    // @ts-expect-error idk how to type, it can be a couple of interaction types
     await context.execute(interaction);
   } catch (error) {
     console.error(error);
